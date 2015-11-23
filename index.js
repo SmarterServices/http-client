faf1dfb5440e0d527f2d67c087e47fb5454056ef
jordan piepkow <jpiepkow@gmail.com>
2015-11-23 14:56:34 -0500

    first push

---
+++ index.js
@@ -0,0 +1,39 @@
+var request = require('request');
+var config = require('./config.json');
+var uuid = require('uuid');
+var nest = require('nestcheck');
+module.exports = function(Obj,callback) {
+    var delay = config.delay.split(',').map(function(i) {
+        return parseInt(i,10);
+    });
+        Obj.timeout = config.timeout;
+    var i = 0;
+    function makeReq() {
+        if(!Obj.headers) {
+            Obj.headers = {}
+        }
+        Obj.headers = {
+            "attempt": i+1,
+            "max_retry": delay.length +1,
+            "tracking_id": uuid.v1()
+
+        };
+        request(Obj,function(err,body,r) {
+            if(nest.get(err,'code') === 'ETIMEDOUT') {
+                retryRequest(i);
+            } else {
+                return callback(err,r);
+            }
+        })
+    }
+    makeReq();
+    function retryRequest(count) {
+        if( count < delay.length) {
+            setTimeout(function(){makeReq()},delay[count]);
+        } else {
+            return callback({err:'Max amount of retries hit'},null)
+        }
+        i++;
+    }
+
+};
---
+++ readme.md
@@ -0,0 +1,25 @@
+Http-client
+===
+##Getting Started:
+
+	npm i @smarterservices/http-client --save
+	
+##Usage:
+	var httpClient = require('http-client');
+	
+	httpClient(REQUESTOBJECT,function(err,r) {
+
+	Console.log('Results returned here!')
+	});
+	
+The first argument passed to httpClient is a request object
+
+####To find what can be passed in to this object look <a href="https://www.npmjs.com/package/request">Here</a>	
+
+The second argument is the callback for returning the results.
+
+##Configurations
+
+Current version of Http-client supports **Retry**
+
+To change the delay,retry limit or timeout go to config.json in the module folder after installation
